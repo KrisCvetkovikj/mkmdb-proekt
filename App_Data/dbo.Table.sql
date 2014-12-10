10a40ad6b63534c1e16992aa18e80e4e0ce55bea
TomzK <Tomce_kirov2008@hotmail.com>
2014-12-10 18:02:30 +0100

    Added Database

---
+++ App_Data/dbo.Table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] VARCHAR(50) NOT NULL, 
+    [Synopsis] VARCHAR(MAX) NULL, 
+    [Genre] VARCHAR(MAX) NULL, 
+    [Actors] VARCHAR(MAX) NULL, 
+    [Rating] FLOAT NULL, 
+    [Poster] VARCHAR(50) NULL, 
+    [Movie Length] INT NULL, 
+    [Release Date] DATE NULL
+)
